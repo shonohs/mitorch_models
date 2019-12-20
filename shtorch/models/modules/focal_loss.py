290ae260e74d600dc28197c5097ccfcf34fb9807
shono <>
2019-12-19 23:36:50 -0800

    minor bug fix.

--- shtorch/models/modules/focal_loss.py
+++ shtorch/models/modules/focal_loss.py
@@ -15,13 +15,20 @@ class FocalLoss(SSDLoss):
         Args:
             pred_classification: (N, num_priors, num_classes)
             target_classification: (N, num_priors)
+
+        Note that for the target classification, "0" represents the background.
         """
         assert len(pred_classification.shape) == 3 and pred_classification.shape[2] == self.num_classes
         assert len(target_classification.shape) == 2 and pred_classification.shape[0:2] == target_classification.shape[0:2]
 
         # Multi-label
-        target = torch.zeros_like(pred_classification)
+        target_shape = pred_classification.shape
+        target_shape = (target_shape[0], target_shape[1], target_shape[2] + 1)
+        target = torch.zeros(target_shape, dtype=pred_classification.dtype, layout=pred_classification.layout, device=pred_classification.device)
         target.scatter_(2, target_classification.unsqueeze(-1), 1)
+        target = target[:,:,1:]
+
+        assert pred_classification.shape == target.shape
         ce = torch.nn.functional.binary_cross_entropy_with_logits(pred_classification, target, reduction='none')
         pt = torch.exp(-ce)
         return (ce * self.alpha * ((1 - pt) ** self.gamma)).sum()
