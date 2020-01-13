d3986a48d0fdb33de5412df9c67bdc42caffdbb4
shono <>
2020-01-13 13:53:22 -0800

    Bug fixes for FPN.

--- shtorch/models/feature_pyramid_network.py
+++ shtorch/models/feature_pyramid_network.py
@@ -1,12 +1,13 @@
 import torch
 from .model import Model
+from .modules import Add
 
 
 class FeaturePyramidNetwork(Model):
     BASE_FEATURE_NAMES = {'EfficientNetB0': ['features.conv1', 'features.block5_0.conv0', 'features.block4_0.conv'],
                           'MobileNetV2': ['features.conv1', 'features.block5_0.conv0', 'features.block3_0.conv0'], # stride 32, 16, 8
                           'MobileNetV3': ['features.conv1', 'features.block4_0.conv0', 'features.block3_0.conv0'],
-                          'MobileNetV3Small': ['features.conv2', 'features.block3_0.conv0', 'features.block2_0.conv0'],
+                          'MobileNetV3Small': ['features.conv1', 'features.block3_0.conv0', 'features.block2_0.conv0'],
                           'SEResNext50': ['features.block3_2', 'features.block3_0.conv0', 'features.block2_0.conv0'],
                           'ShuffleNetV2': ['features.conv1', 'features.block2_0.conv0', 'features.block1_0.conv0'],
                           'VGG16ForSSD': ['features.conv5_1', 'features.conv3_2', 'features.conv2_2']
@@ -34,24 +35,31 @@ class FeaturePyramidNetwork(Model):
 
         self.conv4 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=2) # P_7
 
+        self.add0 = Add()
+        self.add1 = Add()
         self.activation = torch.nn.ReLU()
 
     def forward(self, input):
         base_features = self.base_model(input, self.base_feature_names)
 
-        x = self.conv0_0(base_features[0])
+        # Coarsest first. C5, C4, C3
+        assert [b.shape[2] for b in base_features] == sorted([b.shape[2] for b in base_features])
+        assert [b.shape[3] for b in base_features] == sorted([b.shape[3] for b in base_features])
+
+        x = self.conv0_0(base_features[0]) # C5
         x = self.upsample0(x)
         p5 = self.conv0_1(x)
 
-        x = self.conv1_0(base_features[1])
+        x = self.add0(x, self.conv1_0(base_features[1])) # C4
         x = self.upsample1(x)
         p4 = self.conv1_1(x)
 
-        x = self.conv2_0(base_features[2])
+        x = self.add1(x, self.conv2_0(base_features[2])) # C3
         x = self.upsample2(x)
         p3 = self.conv2_1(x)
 
-        p6 = self.conv3(base_features[0])
+        p6 = self.conv3(base_features[0]) # C5
+
         x = self.activation(p6)
         p7 = self.conv4(x)
 
