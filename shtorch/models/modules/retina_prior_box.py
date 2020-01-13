74e4ad3572fd787991a009c242447aebbc711036
shono <>
2020-01-13 13:30:22 -0800

    Minor refactoring for prior boxes.

--- shtorch/models/modules/retina_prior_box.py
+++ shtorch/models/modules/retina_prior_box.py
@@ -14,25 +14,13 @@ class RetinaPriorBox(PriorBox):
     def get_num_priors(self):
         return [len(self.aspect_ratios) * len(self.anchor_scales)] * self.num_scales
 
-    def forward(self, input):
-        assert len(input) == self.num_scales
-
-        # Get spatial shape of each features.
-        shapes = [i[0].shape[2:] for i in input]
+    def _make_boxes(self, cx, cy, index):
+        size = self.min_size * (2 ** index)
 
         prior_boxes = []
-        for i, shape in enumerate(shapes):
-            for y in range(shape[0]):
-                for x in range(shape[1]):
-                    cx = (x + 0.5) / shape[1]
-                    cy = (y + 0.5) / shape[0]
+        for aspect_ratio in self.aspect_ratios:
+            sqrt_ar = math.sqrt(aspect_ratio)
+            for scale in self.anchor_scales:
+                prior_boxes.append(PriorBox._to_corners(cx, cy, size * scale * sqrt_ar, size * scale / sqrt_ar))
 
-                    size = self.min_size * (2 ** i)
-
-                    for aspect_ratio in self.aspect_ratios:
-                        sqrt_ar = math.sqrt(aspect_ratio)
-                        for scale in self.anchor_scales:
-                            prior_boxes.append(PriorBox._to_corners(cx, cy, size * scale * sqrt_ar, size * scale / sqrt_ar))
-
-        prior_boxes = torch.Tensor(prior_boxes).detach().to(input[0][0].device)
-        return torch.clamp(prior_boxes, min=0, max=1)
+        return prior_boxes
